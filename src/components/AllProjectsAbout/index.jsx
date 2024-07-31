61e4bbe53ebdc5bf8732f26839888ad2de950ecc
Florent Cuer <florentcuer@gmail.com>
2024-07-31 16:47:18 +0100

    update

--- src/components/AllProjectsAbout/index.jsx
+++ src/components/AllProjectsAbout/index.jsx
@@ -9,7 +9,7 @@ export default function AllProjectsAbout() {
             <div className="flex flex-col text-grey">
                 <h1 className="w-full text-right font-bold text-4xl font-['Outfit'] md:text-3xl md:text-left sm:text-2xl">Projects</h1>
                 <h2 className="w-full text-right font-bold text-lg font-['Outfit']"></h2>
-                <p className="w-full pt-4 text-justify font-['Playwrite_DE_Grund'] md:pt-2 sm:text-sm">My front-end projects are built using React and tailwindcss, while my back-end projects are built using PHP and MySQL.</p>
+                <p className="w-full pt-4 text-justify font-['Playwrite_DE_Grund'] md:pt-2 sm:text-sm">My frontend projects are built using React and tailwindcss, while my backend projects are built using PHP and MySQL.</p>
             </div>
             <a href="#project1" className="flex flex-col w-full mt-12 text-grey hover:opacity-70 hover:text-blueGrey lg:hidden">
                 <h3 className="w-full text-right font-semibold text-3xl font-['Outfit']">Project 1</h3>
