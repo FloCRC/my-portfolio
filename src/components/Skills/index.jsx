1c1ecabe5e8b91c2ae31db0f0ef27135786b4e38
Florent Cuer <florentcuer@gmail.com>
2024-09-09 15:47:59 +0100

    rearranged skills and added updated CV

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -5,17 +5,17 @@ export default function Skills() {
             <div href="#home" className="text-grey md:pt-2">
                 <h2 className="text-right font-bold text-3xl font-['Outfit'] md:text-2xl md:text-left">Skills</h2>
                 <div className="grid grid-cols-4 gap-y-8 gap-x-12 py-8 text-center lg:py-4 sm:grid-cols-3 lg:gap-y-6 sm:gap-y-2 sm:py-2">
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-laravel text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />Laravel</p>
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-php text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />PHP</p>
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-solid fa-gear text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />MySQL</p>
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-solid fa-database text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />SQL</p>
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-react text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />React</p>
+                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-js text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />JS</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-html5 text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />HTML</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-css3-alt text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />CSS</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-js text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />JS</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-solid fa-wind text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />tailwind</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-react text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />React</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-git text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />Git</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-github text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />GitHub</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-php text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />PHP</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-laravel text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />Laravel</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-solid fa-database text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />SQL</p>
-                    <p className="font-['Outfit'] sm:text-sm"><i className="fa-solid fa-gear text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />MySQL</p>
                     <p className="font-['Outfit'] sm:text-sm"><i className="fa-brands fa-docker text-6xl text-red lg:text-4xl sm:text-2xl"></i><br />Docker</p>
                 </div>
             </div>
